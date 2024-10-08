9f5eec73d52391a727057bfde1f40ecb20270b6b
Sachin Shetkar <ssshetkar2001@gmail.com>
2024-10-08 17:06:04 +0530

    Creating Bean by using @Component Annotation

---
+++ Day01/introductionToSpringBoot/src/main/java/com/sachin/week1Introduction/introductionToSpringBoot/Apple.java
@@ -0,0 +1,11 @@
+package com.sachin.week1Introduction.introductionToSpringBoot;
+
+//import org.springframework.stereotype.Component;
+
+//@Component
+public class Apple {
+
+    void eatApple(){
+        System.out.println("I am eating the apple");
+    }
+}
--- Day01/introductionToSpringBoot/src/main/java/com/sachin/week1Introduction/introductionToSpringBoot/IntroductionToSpringBootApplication.java
+++ Day01/introductionToSpringBoot/src/main/java/com/sachin/week1Introduction/introductionToSpringBoot/IntroductionToSpringBootApplication.java
@@ -1,12 +1,26 @@
 package com.sachin.week1Introduction.introductionToSpringBoot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IntroductionToSpringBootApplication {
+public class IntroductionToSpringBootApplication implements ApplicationRunner {
+
+	@Autowired
+	Apple obj;
 
 	public static void main(String[] args) {
 		SpringApplication.run(IntroductionToSpringBootApplication.class, args);
+	}
+
+
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		obj.eatApple();
 	}
 }
