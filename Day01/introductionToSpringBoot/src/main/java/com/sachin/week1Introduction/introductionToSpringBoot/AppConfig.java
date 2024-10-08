8f2f16c19fb500666c10655dbffe811f5471be50
Sachin Shetkar <ssshetkar2001@gmail.com>
2024-10-08 17:06:38 +0530

    Creating Bean Using @Configuration @Bean annotations

---
+++ Day01/introductionToSpringBoot/src/main/java/com/sachin/week1Introduction/introductionToSpringBoot/AppConfig.java
@@ -0,0 +1,13 @@
+package com.sachin.week1Introduction.introductionToSpringBoot;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    Apple getApple(){
+        return new Apple();
+    }
+}
