f0bc3504e4b2dface6b55862efffccbeee0e17e5
Sachin Shetkar <ssshetkar2001@gmail.com>
2024-10-08 16:20:26 +0530

    1.1 Setting Up SpringBoot Project

---
+++ Day01/introductionToSpringBoot/src/main/java/com/sachin/week1Introduction/introductionToSpringBoot/IntroductionToSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.sachin.week1Introduction.introductionToSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionToSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionToSpringBootApplication.class, args);
+	}
+}
---
+++ Day01/introductionToSpringBoot/src/test/java/com/sachin/week1Introduction/introductionToSpringBoot/IntroductionToSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachin.week1Introduction.introductionToSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionToSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
